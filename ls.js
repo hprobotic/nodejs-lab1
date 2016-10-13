5b3d2d80028297f32ad0c115ebfae15c86281177
JohnP <hprobotic@gmail.com>
2016-10-13 19:28:00 +0700

    Fix async func bug

--- ls.js
+++ ls.js
@@ -1,10 +1,12 @@
 require('./helper')
+let fs = require('fs').promise
 
-async function ls() {
-  // Use 'await' inside 'async function's
-  console.log('Executing ls function...')
-
-  // Your implementation here
+function lsPromise() {
+    return new Promise((resolve, reject) => {
+        console.log('executing ls function...');
+        
+        resolve();
+    });
 }
 
-ls()
+lsPromise();
