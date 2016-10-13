92770dd48ff595469af57eea06aa0e6c2398bd07
vagit <gracefulgirl207@gmail.com>
2016-10-13 19:54:09 +0700

    milestone 2

--- ls.js
+++ ls.js
@@ -1,12 +1,21 @@
 require('./helper')
 let fs = require('fs').promise
+let {dir} = require('yargs')
+    .default('dir', __dirname)
+    .argv
 
 function lsPromise() {
     return new Promise((resolve, reject) => {
-        console.log('executing ls function...');
-        
-        resolve();
-    });
+        console.log('executing ls function...')
+
+        let promise = fs.readdir(__dirname)
+        let fileNames = await fs.readdir(dir)
+
+        for (let fileName of fileNames) {
+            let filePath = path.join(__dirname, file)
+        }
+        resolve()
+    })
 }
 
 lsPromise();
